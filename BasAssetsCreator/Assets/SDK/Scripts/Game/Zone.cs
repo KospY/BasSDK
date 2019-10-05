257fc744d2fd92ab40afc3381566fd4b46a60ae5
KospY <kospy@warpfrog.com>
2019-10-05 21:38:37 +0200

    Added zone logic

--- BasAssetsCreator/Assets/SDK/Scripts/Game/Zone.cs
+++ BasAssetsCreator/Assets/SDK/Scripts/Game/Zone.cs
@@ -10,8 +10,20 @@ namespace BS
         [Header("Kill")]
         public bool killPlayer;
         public bool killNPC;
+        [Header("Despawn")]
+        public bool despawnNPC;
+        public bool despawnItem;
+        public float despawnDelay = 0;
+        [Header("FX")]
+        public bool spawnFx;
+        public string lightFxID;
+        public float massFxThreshold = 10;
+        public float maxVelocityFxRatio = 8;
+        public string heavyFxID;
+        public Vector3 fxOrientation = Vector3.forward;
         [Header("Teleport")]
         public bool teleportPlayer;
+        public bool teleportItem;
         public Transform customTeleportTarget;
     }
 }
