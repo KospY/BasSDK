6fa4e0ae3c6be086672ec6ac86129d1195be2903
KospY <kospy@warpfrog.com>
2020-05-05 09:55:50 +0200

    Added creature and ragdoll definition

---
+++ Assets/SDK/Scripts/Game/CreatureDefinition.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace BS
+{
+    public class CreatureDefinition : MonoBehaviour
+    {
+        public Animator animator;
+        public Transform meshRootBones;
+    }
+}
---
+++ Assets/SDK/Scripts/Game/RagdollPartDefinition.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BS
+{
+    public class RagdollPartDefinition : MonoBehaviour
+    {
+        public Transform meshBone;
+        [NonSerialized]
+        public Transform animationBone;
+        [NonSerialized]
+        public Rigidbody rb;
+
+        public List<RagdollPartDefinition> ignoredParts;
+
+        protected RagdollDefinition ragdollDefinition;
+
+        public void Awake()
+        {
+            ragdollDefinition = this.GetComponentInParent<RagdollDefinition>();
+            rb = this.GetComponent<Rigidbody>();
+            foreach (RagdollPartDefinition part in ignoredParts)
+            {
+                foreach (Collider thisCollider in this.GetComponentsInChildren<Collider>())
+                {
+                    foreach (Collider ignoredCollider in part.GetComponentsInChildren<Collider>())
+                    {
+                        Physics.IgnoreCollision(thisCollider, ignoredCollider, true);
+                    }
+                }
+            }
+        }
+    }
+}
