56683fa53035d99f01fe789af40b6e2f132462b5
KospY <kospy@warpfrog.com>
2020-06-04 15:32:21 +0200

    Added shuffle mode to audiocontainer

--- Assets/SDK/Scripts/Game/AudioContainer.cs
+++ Assets/SDK/Scripts/Game/AudioContainer.cs
@@ -10,9 +10,16 @@ namespace ThunderRoad
         public List<AudioClip> sounds;
 
         //this list contains all sounds except the last played
-        protected List<AudioClip> filteredSounds;
+        protected AudioClip[] filteredSounds;
 
         protected AudioClip lastPlayedClip = null;
+
+        protected int filteredSoundsCount = 0;
+
+        private void Awake()
+        {
+            filteredSounds = new AudioClip[sounds.Count];
+        }
 
         public AudioClip PickAudioClip()
         {
@@ -26,7 +33,16 @@ namespace ThunderRoad
 
         protected void FilterClips(List<AudioClip> audioClips)
         {
-            filteredSounds = audioClips.Where(x => x != lastPlayedClip).ToList();
+            filteredSoundsCount = 0;
+
+            for (int i = 0; i < sounds.Count; i++)
+            {
+                if (sounds[i] != lastPlayedClip)
+                {
+                    filteredSounds[i] = sounds[i];
+                    filteredSoundsCount++;
+                }
+            }
         }
 
         public AudioClip GetRandomAudioClip(List<AudioClip> audioClips)
@@ -34,7 +50,7 @@ namespace ThunderRoad
             if (audioClips.Count == 0) return null;
             if (audioClips.Count == 1) return audioClips[0];
             FilterClips(sounds);
-            int index = UnityEngine.Random.Range(0, filteredSounds.Count);
+            int index = UnityEngine.Random.Range(0, filteredSoundsCount - 1);
             lastPlayedClip = filteredSounds[index];
             return lastPlayedClip;
         }
