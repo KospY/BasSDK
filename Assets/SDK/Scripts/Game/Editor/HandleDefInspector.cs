e41360e378e37ab94bf2e1ed87a1635e4a5f844e
supermassimo <massimocantugarcia@gmail.com>
2020-06-08 13:32:00 -0300

    Fixed point to point transform for handles
    Added generic Point-to-point script for scripts to inherit from. This is to ensure all point-to-point transforms behave the same

--- Assets/SDK/Scripts/Game/Editor/HandleDefInspector.cs
+++ Assets/SDK/Scripts/Game/Editor/HandleDefInspector.cs
@@ -5,37 +5,16 @@ using UnityEditor;
 namespace ThunderRoad
 {
     [CustomEditor(typeof(HandleDefinition))]
-    public class HandleDefInspector : Editor
+    public class HandleDefInspector : PointToPoint
     {
-        bool toolsHidden = false;
-        Tool previousTool;
-        Vector3 HandlePoint1;
-        Vector3 HandlePoint2;
-        Quaternion axis;
-        bool centerTransform = true;
+        HandleDefinition handle;
 
         public override void OnInspectorGUI()
         {
-
-            HandleDefinition handle = (HandleDefinition)target;
+            script = handle = (HandleDefinition)target;
             ItemDefinition item = handle.transform.GetComponentInParent<ItemDefinition>();
 
-            if (centerTransform)
-            {
-                if (GUILayout.Button("Enable Point to Point transforms"))
-                {
-                    centerTransform = false;
-                    handle.transform.hideFlags = HideFlags.NotEditable;
-                }
-            }
-            else
-            {
-                if (GUILayout.Button("Enable Center Transform"))
-                {
-                    centerTransform = true;
-                    handle.transform.hideFlags = 0;
-                }
-            }
+            PointToPointButton(handle.axisLength);
 
             base.OnInspectorGUI();
 
@@ -82,67 +61,13 @@ namespace ThunderRoad
 
         private void OnEnable()
         {
-            HandleDefinition handle = (HandleDefinition)target;
-
-            HandlePoint1 = handle.transform.position + (handle.transform.up * (handle.axisLength * 0.5f));
-            HandlePoint2 = handle.transform.position + (handle.transform.up * -(handle.axisLength * 0.5f));
-
-            handle.transform.hideFlags = HideFlags.NotEditable;
+            script = handle = (HandleDefinition)target;
+            ResetPoints(handle.axisLength);
         }
         private void OnSceneGUI()
         {
-            HandleDefinition handle = (HandleDefinition)target;
-
-            if (handle.axisLength > 0 && !centerTransform)
-            {
-                if (Tools.current != Tool.None)
-                {
-                    previousTool = Tools.current;
-                    Tools.current = Tool.None;
-                    toolsHidden = true;
-                }
-                HandlePoint1 = Handles.DoPositionHandle(HandlePoint1, Quaternion.LookRotation(handle.transform.forward, handle.transform.up));
-                HandlePoint2 = Handles.DoPositionHandle(HandlePoint2, Quaternion.LookRotation(handle.transform.forward, handle.transform.up));
-                Handles.color = Color.green;
-                try
-                {
-                    if (EditorWindow.mouseOverWindow && EditorWindow.mouseOverWindow.ToString() == " (UnityEditor.SceneView)")
-                    {
-                        handle.transform.position = Vector3.Lerp(HandlePoint1, HandlePoint2, 0.5f);
-                        handle.axisLength = (HandlePoint1 - HandlePoint2).magnitude;
-
-                        if (Event.current.control)
-                        {
-                            axis = Handles.Disc(handle.transform.rotation, handle.transform.position, (HandlePoint1 - HandlePoint2), HandleUtility.GetHandleSize(handle.transform.position), false, 15f);
-                        }
-                        else
-                        {
-                            axis = Handles.Disc(handle.transform.rotation, handle.transform.position, (HandlePoint1 - HandlePoint2), HandleUtility.GetHandleSize(handle.transform.position), false, 0.1f);
-                        }
-                        handle.transform.rotation = Quaternion.LookRotation(HandlePoint2 - HandlePoint1, axis * Vector3.forward) * Quaternion.AngleAxis(-90, Vector3.right);
-                    }
-                    else
-                    {
-                        axis = Handles.Disc(handle.transform.rotation, handle.transform.position, (HandlePoint1 - HandlePoint2), HandleUtility.GetHandleSize(handle.transform.position), false, 0.1f);
-                    }
-                }
-                catch (Exception) { }
-            }
-            else if (toolsHidden)
-            {
-                Tools.current = previousTool;
-                toolsHidden = false;
-            }
-        }
-
-        private void OnDisable()
-        {
-            EditorUtility.SetDirty(target);
-            if (toolsHidden)
-            {
-                Tools.current = previousTool;
-                toolsHidden = false;
-            }
+            script = handle = (HandleDefinition)target;
+            UpdatePoints(ref handle.axisLength);
         }
     }
 }
---
+++ Assets/SDK/Scripts/Game/Editor/PointToPoint.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+namespace ThunderRoad
+{
+    public class PointToPoint : Editor
+    {
+        protected Vector3 lengthPoint1;
+        protected Vector3 lengthPoint2;
+        protected Quaternion axis;
+        protected bool toolsHidden = false;
+        protected bool centerTransform = true;
+        protected Tool previousTool;
+        protected bool centerRotation;
+        protected MonoBehaviour script;
+
+        protected void PointToPointButton(float length)
+        {
+            if (length > 0)
+            {
+                if (centerTransform)
+                {
+                    if (GUILayout.Button("Enable Point to Point transforms"))
+                    {
+                        centerTransform = false;
+                    }
+                }
+                else
+                {
+                    if (GUILayout.Button("Enable Center Transform"))
+                    {
+                        centerTransform = true;
+                        script.transform.hideFlags = 0;
+                    }
+                }
+            }
+            else
+            {
+                if (!centerTransform)
+                {
+                    centerTransform = true;
+                }
+            }
+            if (!centerTransform)
+            {
+                ResetPoints(length);
+            }
+        }
+
+        protected void ResetPoints(float length)
+        {
+            lengthPoint1 = script.transform.position + (script.transform.up * (length * 0.5f));
+            lengthPoint2 = script.transform.position + (script.transform.up * -(length * 0.5f));
+        }
+
+        protected void UpdatePoints(ref float length)
+        {
+            if (length > 0 && !centerTransform)
+            {
+                if (Tools.current != Tool.None)
+                {
+                    previousTool = Tools.current;
+                    Tools.current = Tool.None;
+                    toolsHidden = true;
+                }
+                EditorGUI.BeginChangeCheck();
+                lengthPoint1 = Handles.DoPositionHandle(lengthPoint1, Quaternion.LookRotation(Vector3.forward, Vector3.up));
+                lengthPoint2 = Handles.DoPositionHandle(lengthPoint2, Quaternion.LookRotation(Vector3.forward, Vector3.up));
+                if (EditorGUI.EndChangeCheck())
+                {
+                    Undo.RecordObject(target, "Moved Handle");
+                }
+                Handles.color = Color.green;
+                try
+                {
+                    if (EditorWindow.focusedWindow && EditorWindow.focusedWindow.ToString() == " (UnityEditor.SceneView)")
+                    {
+                        script.transform.position = Vector3.Lerp(lengthPoint1, lengthPoint2, 0.5f);
+                        length = (lengthPoint1 - lengthPoint2).magnitude;
+
+                        if (Event.current.control)
+                        {
+                            axis = Handles.Disc(script.transform.rotation, script.transform.position, (lengthPoint1 - lengthPoint2), HandleUtility.GetHandleSize(script.transform.position), false, 15f);
+                        }
+                        else
+                        {
+                            axis = Handles.Disc(script.transform.rotation, script.transform.position, (lengthPoint1 - lengthPoint2), HandleUtility.GetHandleSize(script.transform.position), false, 0.1f);
+                        }
+                        script.transform.rotation = Quaternion.LookRotation(lengthPoint2 - lengthPoint1, axis * Vector3.forward) * Quaternion.AngleAxis(-90, Vector3.right);
+                    }
+                    else
+                    {
+                        axis = Handles.Disc(script.transform.rotation, script.transform.position, (lengthPoint1 - lengthPoint2), HandleUtility.GetHandleSize(script.transform.position), false, 0.1f);
+                    }
+                }
+                catch (Exception) { }
+            }
+            else if (toolsHidden)
+            {
+                Tools.current = previousTool;
+                toolsHidden = false;
+            }
+        }
+        
+        public void OnDisable()
+        {
+            if (target) EditorUtility.SetDirty(target);
+            if (toolsHidden)
+            {
+                Tools.current = previousTool;
+                toolsHidden = false;
+            }
+        }
+
+    }
+}
