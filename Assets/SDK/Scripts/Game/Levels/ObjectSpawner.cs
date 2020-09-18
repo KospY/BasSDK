c0c3bdb0f112b4e9671ec9717a3649fcd64ff4ef
KospY <kospy@warpfrog.com>
2020-09-18 18:00:48 +0200

    Added addressable compatible ObjectSpawner

---
+++ Assets/SDK/Scripts/Game/Levels/ObjectSpawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.AddressableAssets;
+#if ODIN_INSPECTOR
+using Sirenix.OdinInspector;
+#endif
+
+namespace ThunderRoad
+{
+    public class ObjectSpawner : MonoBehaviour
+    {
+        public string address;
+        public bool spawnOnStart = true;
+        public bool disableOnMobile;
+
+        protected void Start()
+        {
+            if (spawnOnStart)
+            {
+                Spawn();
+            }
+        }
+
+        [Button]
+        public void Spawn()
+        {
+            if (disableOnMobile && Application.isMobilePlatform) return;
+            Addressables.InstantiateAsync(address, this.transform.position, this.transform.rotation, this.transform);
+        }
+    }
+}
