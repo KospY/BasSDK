fd0f5d3a73fb06addba465acff928fb27bfe9396
KospY <kospy@warpfrog.com>
2020-10-16 18:51:19 +0200

    Added footstep component

---
+++ Assets/SDK/Scripts/Game/Creatures/Footstep.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace ThunderRoad
+{
+    [RequireComponent(typeof(SphereCollider))]
+    public class Footstep : MonoBehaviour
+    {
+        public string materialId = "Footstep";
+        public float maxVelocity = 10;
+
+    }
+}
