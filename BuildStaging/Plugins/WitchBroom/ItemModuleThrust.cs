0903816d86607ee47ba9bf0ead19697a970a1feb
KospY <kospy@warpfrog.com>
2020-11-01 23:33:09 +0100

    Updated WitchBroom example for 8.4

--- BuildStaging/Plugins/WitchBroom/ItemModuleThrust.cs
+++ BuildStaging/Plugins/WitchBroom/ItemModuleThrust.cs
@@ -1,12 +1,14 @@
-﻿using BS;
+﻿using UnityEngine;
+using ThunderRoad;
 
-namespace BasPluginExample
+namespace WitchBroom
 {
     // This create an item module that can be referenced in the item JSON
     public class ItemModuleThrust : ItemModule
     {
         public float minForce = 500;
         public float maxForce = 1500;
+        public string effectId;
 
         public override void OnItemLoaded(Item item)
         {
