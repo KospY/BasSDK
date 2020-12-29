fa41fbf6b69e37d1642e049683dee0eaef89da67
KospY <kospy@warpfrog.com>
2020-12-29 14:39:39 +0100

    Added container spawner

---
+++ Assets/SDK/Scripts/Game/Items/ContainerSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+#if ODIN_INSPECTOR
+using Sirenix.OdinInspector;
+#endif
+
+namespace ThunderRoad
+{
+    public class ContainerSpawner : MonoBehaviour
+    {
+        public string containerId;
+        public bool pooled;
+        public bool spawnOnStart = true;
+
+        public bool spawnOnTransform = false;
+
+        public List<Transform> transformSpawnPoints = new List<Transform>();
+
+    }
+}
