6afd0aa30abf0acfe4287ade4632d7111b5910a8
KospY <kospy@warpfrog.com>
2021-02-13 11:21:55 +0100

    Added dungen support

--- Assets/SDK/Scripts/Game/PlayerGrabTest.cs
+++ Assets/SDK/Scripts/Game/PlayerGrabTest.cs
@@ -3,84 +3,145 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 
-public class PlayerGrabTest : MonoBehaviour
+namespace ThunderRoad
 {
-    public XRNode xrNode;
-    protected Transform grabbedTransform;
-    protected Rigidbody grabbedRb;
-    protected bool orgKinematic;
-    protected InputDevice device;
-    protected bool pressState;
 
-    private void Start()
+    public class PlayerGrabTest : MonoBehaviour
     {
-        var devices = new List<InputDevice>();
-        InputDevices.GetDevicesAtXRNode(xrNode, devices);
+        public XRNode xrNode;
+        protected Rigidbody rb;
+        protected FixedJoint fixedJoint;
+        protected Rigidbody grabbedRb;
+        protected bool orgKinematic;
+        protected InputDevice device;
 
-        if (devices.Count == 1)
-        {
-            device = devices[0];
-            Debug.Log(string.Format("Device name '{0}' with role '{1}'", device.name, device.role.ToString()));
-        }
-        else if (devices.Count > 1)
-        {
-            Debug.Log("Found more than one left hand!");
-        }
-    }
+        protected bool primaryPressState;
+        protected bool secondaryPressState;
+        protected bool gripPressState;
 
-    void Update()
-    {
-        bool pressed;
-        if (device.TryGetFeatureValue(CommonUsages.gripButton, out pressed))
+        private void Awake()
         {
-            if (pressed)
+            rb = this.GetComponent<Rigidbody>();
+            if (!rb) rb = this.gameObject.AddComponent<Rigidbody>();
+            rb.isKinematic = true;
+        }
+
+        private void Start()
+        {
+            var devices = new List<InputDevice>();
+            InputDevices.GetDevicesAtXRNode(xrNode, devices);
+
+            if (devices.Count == 1)
             {
-                if (!pressState)
+                device = devices[0];
+                Debug.Log(string.Format("Device name '{0}' with role '{1}'", device.name, device.role.ToString()));
+            }
+            else if (devices.Count > 1)
+            {
+                Debug.Log("Found more than one left hand!");
+            }
+        }
+
+        void Update()
+        {
+
+#if DUNGEN
+            if (xrNode == XRNode.LeftHand)
+            {
+                if (device.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryPressed))
                 {
-                    if (!grabbedTransform)
+                    if (primaryPressed)
                     {
-                        Collider[] colliders = Physics.OverlapSphere(this.transform.position, 0.05f);
-                        float closestDistanceSqr = Mathf.Infinity;
-                        Collider nearestCollider = null;
-                        foreach (Collider collider in colliders)
+                        if (!primaryPressState)
                         {
-                            float dSqrToTarget = (collider.ClosestPoint(this.transform.position) - this.transform.position).sqrMagnitude;
-                            if (dSqrToTarget < closestDistanceSqr)
-                            {
-                                closestDistanceSqr = dSqrToTarget;
-                                nearestCollider = collider;
-                            }
-                        }
-                        if (nearestCollider)
-                        {
-                            if (nearestCollider.attachedRigidbody)
-                            {
-                                orgKinematic = nearestCollider.attachedRigidbody.isKinematic;
-                                grabbedTransform = nearestCollider.attachedRigidbody.transform;
-                            }
-                            else
-                            {
-                                grabbedTransform = nearestCollider.transform.root;
-                            }       
-                            grabbedTransform.SetParent(this.transform);
-                            this.GetComponent<MeshRenderer>().enabled = false;
+                            PlayerControllerTest playerControllerTest = this.GetComponentInParent<PlayerControllerTest>();
+                            DunGen.AdjacentRoomCulling adjacentRoomCulling = playerControllerTest.head.gameObject.GetComponent<DunGen.AdjacentRoomCulling>();
+                            adjacentRoomCulling.enabled = !adjacentRoomCulling.enabled;
+                            primaryPressState = true;
                         }
                     }
-                    pressState = true;
+                    else
+                    {
+                        if (primaryPressState)
+                        {
+                            primaryPressState = false;
+                        }
+                    }
+                }
+                if (device.TryGetFeatureValue(CommonUsages.secondaryButton, out bool secondaryPressed))
+                {
+                    if (secondaryPressed)
+                    {
+                        if (!secondaryPressState)
+                        {
+                            Level level = GameObject.FindObjectOfType<Level>();
+                            if (level && level.dungeonGenerator)
+                            {
+                                level.GenerateDungeon();
+                            }
+                            secondaryPressState = true;
+                        }
+                    }
+                    else
+                    {
+                        if (secondaryPressState)
+                        {
+                            secondaryPressState = false;
+                        }
+                    }
                 }
             }
-            else
+#endif
+            if (device.TryGetFeatureValue(CommonUsages.gripButton, out bool gripPressed))
             {
-                if (pressState)
+                if (gripPressed)
                 {
-                    if (grabbedTransform)
+                    if (!gripPressState)
                     {
-                        if (grabbedRb) grabbedRb.isKinematic = orgKinematic;
-                        grabbedTransform.SetParent(null);
-                        grabbedTransform = null;
-                        this.GetComponent<MeshRenderer>().enabled = true;
+                        if (!grabbedRb)
+                        {
+                            Collider[] colliders = Physics.OverlapSphere(this.transform.position, 0.05f);
+                            float closestDistanceSqr = Mathf.Infinity;
+                            Collider nearestCollider = null;
+                            foreach (Collider collider in colliders)
+                            {
+                                if (collider.attachedRigidbody)
+                                {
+                                    float dSqrToTarget = (collider.ClosestPoint(this.transform.position) - this.transform.position).sqrMagnitude;
+                                    if (dSqrToTarget < closestDistanceSqr)
+                                    {
+                                        closestDistanceSqr = dSqrToTarget;
+                                        nearestCollider = collider;
+                                    }
+                                }
+                            }
+                            if (nearestCollider)
+                            {
+                                if (nearestCollider.attachedRigidbody)
+                                {
+                                    //orgKinematic = nearestCollider.attachedRigidbody.isKinematic;
+                                    grabbedRb = nearestCollider.attachedRigidbody;
+                                    fixedJoint = rb.gameObject.AddComponent<FixedJoint>();
+                                    fixedJoint.connectedBody = grabbedRb;
+                                    this.GetComponent<MeshRenderer>().enabled = false;
+                                }
+                            }
+                        }
+                        gripPressState = true;
                     }
-                    pressState = false;
+                }
+                else
+                {
+                    if (gripPressState)
+                    {
+                        if (grabbedRb)
+                        {
+                            if (fixedJoint) Destroy(fixedJoint);
+                            grabbedRb = null;
+                            this.GetComponent<MeshRenderer>().enabled = true;
+                        }
+                        gripPressState = false;
+                    }
                 }
             }
         }
