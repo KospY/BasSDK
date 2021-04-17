daf1497419fa7f02c42bc1ce699aa63cea34bd73
KospY <kospy@warpfrog.com>
2021-04-18 01:20:31 +0200

    Added disableonplay script for doorblocker

---
+++ Assets/SDK/Scripts/Levels/DisableOnPlay.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace ThunderRoad
+{
+    [AddComponentMenu("ThunderRoad/Levels/Disable on play")]
+    public class DisableOnPlay : MonoBehaviour
+    {
+        protected void Awake()
+        {
+            this.gameObject.SetActive(false);
+        }
+
+    }
+}
