699389b53a3622bef9f63c71e50f6d81a6cd40e9
KospY <kospy@warpfrog.com>
2021-05-06 10:57:25 +0200

    Added effect mixer

---
+++ Assets/SDK/Scripts/Effects/New/EffectMixer.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+#if ODIN_INSPECTOR
+using Sirenix.OdinInspector;
+#else
+using EasyButtons;
+#endif
+
+namespace ThunderRoad
+{
+    public class EffectMixer : MonoBehaviour
+    {
+        [Range(0, 1)]
+        public float intensity;
+        [Range(0, 1)]
+        public float speed;
+
+        public bool playOnStart = false;
+
+        public List<EffectMixerModule> modules;
+
+        protected bool initialized;
+
+        private void OnValidate()
+        {
+            modules = new List<EffectMixerModule>(this.GetComponentsInChildren<EffectMixerModule>());
+            if (Application.isPlaying && initialized)
+            {
+                Refresh();
+            }
+        }
+
+        private void Start()
+        {
+            if (playOnStart) Play();
+            initialized = true;
+        }
+
+        [Button]
+        public void Play()
+        {
+            Refresh();
+            foreach (EffectMixerModule module in modules)
+            {
+                module.Play();
+            }
+        }
+
+        public void Refresh()
+        {
+            foreach (EffectMixerModule module in modules)
+            {
+                module.SetIntensity(intensity);
+                module.SetSpeed(speed);
+            }
+        }
+
+        [Button]
+        public void Stop()
+        {
+            foreach (EffectMixerModule module in modules)
+            {
+                module.Stop();
+            }
+        }
+    }
+}
---
+++ Assets/SDK/Scripts/Effects/New/EffectMixerModule.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace ThunderRoad
+{
+    public class EffectMixerModule : MonoBehaviour
+    {
+        public virtual void Play()
+        {
+
+        }
+
+        public virtual void SetIntensity(float intensity)
+        {
+
+        }
+
+        public virtual void SetSpeed(float speed)
+        {
+
+        }
+
+        public virtual void Stop()
+        {
+
+        }
+    }
+}
