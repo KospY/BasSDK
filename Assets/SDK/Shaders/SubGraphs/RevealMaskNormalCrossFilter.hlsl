4812ccf989293139041817daa30a5d36dc8fb909
kenamis <kenan.chabuk@gmail.com>
2021-05-12 19:43:08 -0700

    added base map tiling to Lit_Reveal

--- Assets/SDK/Shaders/SubGraphs/RevealMaskNormalCrossFilter.hlsl
+++ Assets/SDK/Shaders/SubGraphs/RevealMaskNormalCrossFilter.hlsl
@@ -1,6 +1,6 @@
 void RevealMaskNormalCrossFilter_float(float2 uv, float height, float texWidth, float texHeight, out float3 Out)
 {
-	float2 texelSize = float2(1 / texWidth, 1 / texHeight);
+	float2 texelSize = float2(1.0 / texWidth, 1.0 / texHeight);
 	float4 h;
 	h[0] = saturate(SAMPLE_TEXTURE2D(_RevealMask, sampler_RevealMask, uv + float2(0, -1) * texelSize).b) * height;
 	h[1] = saturate(SAMPLE_TEXTURE2D(_RevealMask, sampler_RevealMask, uv + float2(-1, 0) * texelSize).b) * height;
