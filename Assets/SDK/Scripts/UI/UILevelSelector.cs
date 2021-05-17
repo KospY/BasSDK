52e8221d324143dcb98c54275a2b85387dc0cf75
KospY <kospy@warpfrog.com>
2021-05-17 10:50:52 +0200

    Added new UI Level selector

---
+++ Assets/SDK/Scripts/UI/UILevelSelector.cs
@@ -0,0 +1,51 @@
+﻿using Sirenix.OdinInspector;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.UI;
+
+namespace ThunderRoad
+{
+    public class UILevelSelector : MonoBehaviour
+    {
+        public string mapId = "Default";
+        public Transform locationRoot;
+        public ToggleGroup orbGroup;
+        public Transform canvasDetails;
+        public Transform wholeMap;
+        public float visibleRadius = 4;
+
+        public Text mapTitleText;
+        public Text modeDescriptionText;
+
+        public RawImage mapSketch = null;
+
+        public Button travelButton = null;
+
+        public Button bulletPointPrefab = null;
+
+
+        protected void OnDrawGizmos()
+        {
+            Gizmos.matrix = this.transform.localToWorldMatrix;
+            Gizmos.DrawWireCube(Vector3.zero, new Vector3(1, 1, 0));
+            /*if (canvasDetails != null)
+            {
+                Gizmos.matrix = canvasDetails.localToWorldMatrix;
+                Gizmos.DrawWireCube(Vector3.zero, new Vector3(0.402f, 0.29f, 0));
+            }*/
+            if (locationRoot != null)
+            {
+                foreach (Transform child in locationRoot)
+                {
+                    Gizmos.matrix = child.localToWorldMatrix;
+                    Gizmos.DrawRay(Vector3.zero, Vector3.back * 0.1f);
+                    Gizmos.DrawWireSphere(new Vector3(0, 0, -0.1f - 0.015f), 0.03f);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/SDK/Scripts/UI/UILevelSelectorLevelMode.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ThunderRoad
+{
+    public class UILevelSelectorLevelMode : UISelectionListButtons
+    {
+        public Text levelModeDescription = null;
+
+    }
+}
