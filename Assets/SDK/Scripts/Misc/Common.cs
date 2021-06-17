d14829a0acc4548b5ff83b3f9e3d1ca133ddfc50
KospY <KospY@warpfrog.com>
2021-06-17 23:37:57 +0200

    Added DrawText method

--- Assets/SDK/Scripts/Misc/Common.cs
+++ Assets/SDK/Scripts/Misc/Common.cs
@@ -210,6 +210,35 @@ namespace ThunderRoad
 #endif
         }
 
+        public static void DrawText(GUISkin guiSkin, string text, Vector3 position, Color? color = null, int fontSize = 0, float yOffset = 0)
+        {
+            var prevSkin = GUI.skin;
+            if (guiSkin == null)
+                Debug.LogWarning("editor warning: guiSkin parameter is null");
+            else
+                GUI.skin = guiSkin;
+
+            GUIContent textContent = new GUIContent(text);
+
+            GUIStyle style = (guiSkin != null) ? new GUIStyle(guiSkin.GetStyle("Label")) : new GUIStyle();
+            if (color != null)
+                style.normal.textColor = (Color)color;
+            if (fontSize > 0)
+                style.fontSize = fontSize;
+
+            Vector2 textSize = style.CalcSize(textContent);
+            Vector3 screenPoint = Camera.current.WorldToScreenPoint(position);
+
+            if (screenPoint.z > 0) // checks necessary to the text is not visible when the camera is pointed in the opposite direction relative to the object
+            {
+                var worldPosition = Camera.current.ScreenToWorldPoint(new Vector3(screenPoint.x - textSize.x * 0.5f, screenPoint.y + textSize.y * 0.5f + yOffset, screenPoint.z));
+#if UNITY_EDITOR
+                UnityEditor.Handles.Label(worldPosition, textContent, style);
+#endif
+            }
+            GUI.skin = prevSkin;
+        }
+
         private static NavMeshPath navMeshPath = new NavMeshPath();
 
         public static T GetClosest<T>(List<T> behaviours, Vector3 position, bool prioritizeShortestPath) where T : Behaviour
