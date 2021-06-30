dfc77296ac785cdd314ae65fea26c0f4a38e16c3
KospY <KospY@warpfrog.com>
2021-06-30 17:17:08 +0200

    Added dynamic rope

---
+++ Assets/SDK/Scripts/Levels/DynamicRope.cs
@@ -0,0 +1,12 @@
+using DunGen;
+using UnityEngine;
+
+namespace ThunderRoad
+{
+    public class DynamicRope : MonoBehaviour
+    {
+        public float raycastRange = 50.0f;
+        public float heightFromGround = 2.0f;
+
+    }
+}
