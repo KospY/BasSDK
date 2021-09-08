b3b8a94a6b0286da3ebd92aed254aaae4b84990b
KospY <KospY@warpfrog.com>
2021-09-08 10:33:52 +0200

    Added FormatBytes

--- Assets/SDK/Scripts/Misc/Extensions.cs
+++ Assets/SDK/Scripts/Misc/Extensions.cs
@@ -43,6 +43,19 @@ namespace ThunderRoad
             return false;
         }
 
+        public static string FormatBytes(this long bytes)
+        {
+            string[] Suffix = { "B", "KB", "MB", "GB", "TB" };
+            int i;
+            double dblSByte = bytes;
+            for (i = 0; i < Suffix.Length && bytes >= 1024; i++, bytes /= 1024)
+            {
+                dblSByte = bytes / 1024.0;
+            }
+
+            return String.Format("{0:0.##} {1}", dblSByte, Suffix[i]);
+        }
+
         public static bool HasFlagNoGC(this RagdollPart.Type flags, RagdollPart.Type value)
         {
             return ((flags & value) > 0);
